8acaccb0d5f1a9d3265d965daeef5159f3aa0b26
Wen Jian <824636515@qq.com>
2016-11-09 18:21:27 +0800

    精选页面的初步完成

---
+++ app/src/main/java/com/douliu/myplayer/ui/adapter/BannerAdapter.java
@@ -0,0 +1,68 @@
+package com.douliu.myplayer.ui.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.douliu.myplayer.bean.VideoType;
+import com.douliu.myplayer.utils.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Banner图的适配器
+ * Created by wenjian on 2016/11/8.
+ */
+public class BannerAdapter extends PagerAdapter{
+
+    private List<VideoType> mVideoInfos;
+
+    private static List<ImageView> mImageList = new ArrayList<>();
+
+    private Context mContext;
+
+    public BannerAdapter(Context ctx,List<VideoType> list) {
+        mVideoInfos = list;
+        mContext = ctx;
+        initImageList();
+    }
+
+    private void initImageList() {
+        for (int i = 0; i < mVideoInfos.size();i++){
+            ImageView imageView = new ImageView(mContext);
+            mImageList.add(imageView);
+        }
+    }
+
+    public void setData(List<VideoType> list){
+        mVideoInfos = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        int pos = position % mVideoInfos.size();
+        ImageView imageView = mImageList.get(pos);
+        ImageLoader.display(container.getContext(),mVideoInfos.get(pos).getPic(),imageView);
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(mImageList.get(position% mVideoInfos.size()));
+    }
+}
